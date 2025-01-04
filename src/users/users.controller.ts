ec7f6722a0f16811766e4fe6987df94527843cd6
himalf <himal.fullel15@gmail.com>
2025-01-04 14:32:54 +0545

    feat: add request response handling with REST API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/users.dto';
+import { UsersService } from './users.service';
+import { ReturningStatementNotSupportedError } from 'typeorm';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post('')
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+  @Get('')
+  async findAll() {
+    return this.usersService.finaAll();
+  }
+  @Get(':id')
+  async findOne(@Param('id') user_id: number) {
+    return this.usersService.findOne(user_id);
+  }
+  @Put(':id')
+  async update(@Param('id') user_id: number, createUserDto: CreateUserDto) {
+    return this.usersService.update(user_id, createUserDto);
+  }
+  @Delete(':id')
+  async remove(@Param('id') user_id: number) {
+    return this.usersService.remove(user_id);
+  }
+}
