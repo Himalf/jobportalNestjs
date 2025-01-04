8dac4d680217612b2ae052f1921c4e7e71333a5c
himalf <himal.fullel15@gmail.com>
2025-01-04 14:07:25 +0545

    feat: add user entity with typorm

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+  @Column()
+  name: string;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  phone_number: number;
+  @Column()
+  role: string;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
