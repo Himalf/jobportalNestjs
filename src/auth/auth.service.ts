7fe2d461c6a2ed9c4886af12e39f48b261be985a
himalf <himal.fullel15@gmail.com>
2025-01-06 12:16:21 +0545

    feat: add validateUser func in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Users } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {
     console.log(jwtService, 'ol');
   }
   async login(user: Users) {
@@ -13,5 +17,8 @@ export class AuthService {
       msg: 'Login Successfull',
       access_token: this.jwtService.sign(payload),
     };
+  }
+  async validateUser(payload: any): Promise<Users> {
+    return this.userService.findByPhone(payload.phone_number);
   }
 }
